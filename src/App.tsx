47267f9644aa243f38018eab8ef792c38594a191
Przemysław Słupski <przemyslaw@slupski.net>
2024-09-03 11:59:14 +0200

    router added

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,26 @@ import "./App.css";
 import QuestionsPage from "./pages/QuestionsPage";
 import Cookies from "js-cookie";
 import md5 from "md5";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import FrontPage from "./pages/FrontPage";
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <FrontPage />,
+    },
+    {
+      path: "/questions",
+      element: <QuestionsPage />
+    }
+  ]);
+
+
   useEffect(() => {
     const a = [window.navigator.userAgent];
     Cookies.set("u", md5(JSON.stringify(a)));
@@ -12,7 +30,7 @@ function App() {
 
   return (
     <div className="App">
-      <QuestionsPage />
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -1,8 +1,5 @@
-import QuestionPanel from "../components/QuestionPanel";
 import React from "react";
 
 export default function FrontPage() {
-    return <div>
-        {/* <QuestionPanel>What do you <span className='dotted-underline'>Think</span>?</QuestionPanel> */}
-    </div>
+    return <h2>What do you <span className='dotted-underline'>Think</span>?</h2>
 }
